b1258e42e85e48d13180247995d8c8a82eac6b60
julian camilo benavides vanegas <correoj94@gmail.com>
2022-11-09 19:28:10 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+const { SERVER_PORT, SERVER_HOST, MONGODB_URI } = process.env;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    app.listen(SERVER_PORT, () => {
+      console.log(`DB connected and Listening on ${SERVER_HOST}:${SERVER_PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
