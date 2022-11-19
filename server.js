f592c93955d8df6da607c29d503de56b48603a63
julian camilo benavides vanegas <correoj94@gmail.com>
2022-11-18 21:23:43 -0500

    post works from service

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const { SERVER_PORT, SERVER_HOST, MONGODB_URI } = process.env;
 
